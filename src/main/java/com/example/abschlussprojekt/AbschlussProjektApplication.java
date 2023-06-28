9477262bed28e5aead5c951dffb32c13795e948e
Gregoire <wi22b060@technikum-wien.at>
2023-06-28 12:08:05 +0200

    basis projekt wurde erstellt

---
+++ src/main/java/com/example/abschlussprojekt/AbschlussProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.abschlussprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlussProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbschlussProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/abschlussprojekt/AbschlussProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abschlussprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlussProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
